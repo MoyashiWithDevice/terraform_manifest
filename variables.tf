e6503515a998d0b00c418303a49e3235279ddc4c
root <YoneyamaYo>
2026-06-12 12:04:29 +0000

    fix property name from "describe" to "description"

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "PROXMOX_VE_ENDPOINT"{
     type = string
-    describe = "Proxmox pve-1 host's endpoint"
+    description = "Proxmox pve-1 host's endpoint"
 }
 variable "PROXMOX_VE_API_TOKEN"{
     type = string
     sensitive = true
-    describe = "Proxmox pve-1 host's API Token"
+    description = "Proxmox pve-1 host's API Token"
 }
 variable "PROXMOX_VE_SSH_USERNAME"{
     type = string
-    describe = "Proxmox pve-1 host's username for creating snippets"
+    description = "Proxmox pve-1 host's username for creating snippets"
 }
 variable "PROXMOX_VE_SSH_PASSWORD"{
     type = string
     sensitive = true
-    describe = "Proxmox pve-1 host's password for creating snippets"
+    description = "Proxmox pve-1 host's password for creating snippets"
 }
