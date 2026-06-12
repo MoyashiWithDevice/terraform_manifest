eaa2c9c3d201b500f85b445ab92a4fdfba6353d6
root <YoneyamaYo>
2026-06-12 11:27:58 +0000

    add import

--- imports.tf
+++ imports.tf
@@ -1,4 +1,55 @@
-import{
-    provider = "proxmox"
-    id = "proxmox"
+# imports.tf
+import {
+  to = proxmox_virtual_environment_vm.vm_100
+  id = "pve-1/vm/100"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_107
+  id = "pve-1/vm/107"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_108
+  id = "pve-1/vm/108"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_111
+  id = "pve-1/vm/111"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_112
+  id = "pve-1/vm/112"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_113
+  id = "pve-1/vm/113"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_114
+  id = "pve-1/vm/114"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_116
+  id = "pve-1/vm/116"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_117
+  id = "pve-1/vm/117"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_118
+  id = "pve-1/vm/118"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.template_9000
+  id = "pve-1/template/9000"
 }
