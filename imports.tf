5ab3513894638d4be499467bbe045c04d410fcb9
root <YoneyamaYo>
2026-06-12 12:23:00 +0000

    fix id in imports.tf

--- imports.tf
+++ imports.tf
@@ -1,55 +1,55 @@
 # imports.tf
 import {
   to = proxmox_virtual_environment_vm.vm_100
-  id = "pve-1/vm/100"
+  id = "100"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_107
-  id = "pve-1/vm/107"
+  id = "107"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_108
-  id = "pve-1/vm/108"
+  id = "108"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_111
-  id = "pve-1/vm/111"
+  id = "111"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_112
-  id = "pve-1/vm/112"
+  id = "112"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_113
-  id = "pve-1/vm/113"
+  id = "113"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_114
-  id = "pve-1/vm/114"
+  id = "114"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_116
-  id = "pve-1/vm/116"
+  id = "116"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_117
-  id = "pve-1/vm/117"
+  id = "117"
 }
 
 import {
   to = proxmox_virtual_environment_vm.vm_118
-  id = "pve-1/vm/118"
+  id = "118"
 }
 
 import {
   to = proxmox_virtual_environment_vm.template_9000
-  id = "pve-1/template/9000"
+  id = "9000"
 }
