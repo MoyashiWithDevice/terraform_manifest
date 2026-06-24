10a21d4ec5a51e759caed11c01ef9389594fb339
bababa <bababa>
2026-06-24 02:08:20 +0000

    add snippet

---
+++ snippet.tf
@@ -0,0 +1,30 @@
+resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name = "pve-1"
+
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    timezone: Asia/Tokyo
+    users:
+      - default
+      - name: ${var.VM_USERNAME}
+        password: ${var.VM_PASSWORD}
+        groups:
+          - sudo
+        shell: /bin/bash
+        sudo: ALL=(ALL:ALL) ALL
+    package_update: true
+    packages:
+      - qemu-guest-agent
+    runcmd:
+      - mkdir -p /etc/ssh
+      - curl -o /etc/ssh/trusted-user-ca-keys.pem http://172.31.0.230:8200/v1/ssh-client-signer/public_key
+      - echo "TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem" | tee -a /etc/ssh/sshd_config
+      - systemctl restart sshd
+    EOF
+
+    file_name = "user-data-cloud-config.yaml"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -16,3 +16,12 @@ variable "PROXMOX_VE_SSH_PASSWORD"{
     sensitive = true
     description = "Proxmox pve-1 host's password for creating snippets"
 }
+variable "VM_USERNAME"{
+    type = string
+    description = "VM's username for creating snippets"
+}
+variable "VM_PASSWORD"{
+    type = string
+    sensitive = true
+    description = "VM's password for creating snippets"
+}
