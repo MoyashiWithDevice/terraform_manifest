ce1ff8f371bee2c754f783f4c301314681d8d753
bababa <bababa>
2026-06-25 21:52:06 +0000

    create test-kratos VM

--- learning.tf
+++ learning.tf
@@ -196,3 +196,111 @@ resource "proxmox_virtual_environment_vm" "vm_112" {
     type = "l26"
   }
 }
+
+resource "proxmox_virtual_environment_vm" "vm_112" {
+  clone{
+    vm_id = proxmox_virtual_environment_vm.template_9000.vm_id
+  } 
+  acpi                                 = true
+  bios                                 = "seabios"
+  boot_order                           = ["scsi0", "ide2", "net0"]
+  delete_unreferenced_disks_on_destroy = true
+  description                          = null
+  hook_script_file_id                  = null
+  keyboard_layout                      = "en-us"
+  kvm_arguments                        = null
+  mac_addresses                        = ["BC:24:11:9C:5D:70"]
+  machine                              = null
+  migrate                              = false
+  name                                 = "test-kratos"
+  network_device = [{
+    enabled      = true
+    bridge       = "vmbr0"
+    disconnected = false
+    firewall     = true
+    mac_address  = ""
+    model        = "virtio"
+    mtu          = 0
+    queues       = 0
+    rate_limit   = 0
+    trunks       = ""
+    vlan_id      = 20
+  }]
+  node_name           = "pve-1"
+  on_boot             = false
+  pool_id             = null
+  protection          = false
+  purge_on_destroy    = true
+  reboot              = false
+  reboot_after_update = false
+  scsi_hardware       = "virtio-scsi-single"
+  started             = false
+  stop_on_destroy     = false
+  tablet_device       = true
+  tags                = []
+  template            = false
+  timeout_clone       = 1800
+  timeout_create      = 1800
+  timeout_migrate     = 1800
+  timeout_reboot      = 1800
+  timeout_shutdown_vm = 1800
+  timeout_start_vm    = 1800
+  timeout_stop_vm     = 300
+  vm_id               = 120
+  agent {
+    enabled = true
+    timeout = "15m"
+    trim    = false
+    type    = "virtio"
+  }
+  cpu {
+    affinity     = null
+    architecture = null
+    cores        = 2
+    flags        = []
+    hotplugged   = 0
+    limit        = 0
+    numa         = false
+    sockets      = 1
+    type         = "qemu64"
+    units        = 1
+  }
+  disk {
+    aio               = "io_uring"
+    backup            = true
+    cache             = "none"
+    datastore_id      = "data"
+    discard           = "ignore"
+    file_format       = "raw"
+    file_id           = null
+    import_from       = null
+    interface         = "scsi0"
+    iothread          = true
+    queues            = 0
+    replicate         = true
+    serial            = null
+    size              = 32
+    ssd               = false
+  }
+  memory {
+    dedicated      = 4096
+    floating       = 4096
+    hugepages      = null
+    keep_hugepages = false
+    shared         = 0
+  }
+  operating_system {
+    type = "l26"
+  }
+  initialization{
+    ip_config{
+      ipv4{
+        address = "dhcp"
+      }
+    }
+    dns{
+      servers = ["172.32.0.5"]
+    }
+    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
+  }
+}
