7196e339f238bf9bd34e26f734f806763de438b6
bababa <pijonsan@gmail.com>
2026-09-21 14:13:48 +0900

    fix

--- modules/snippet/main.tf
+++ modules/snippet/main.tf
@@ -1,29 +1,27 @@
+locals {
+  cloud_config = "#cloud-config\n" ~ yamlencode({
+    timezone = var.tz
+    ssh_pwauth = var.ssh_pwauth
+
+    users = [for u in var.users :{
+      name = u.name
+      groups = u.groups
+      shell = u.shell
+      sudo = u.sudo
+    }]
+    chpasswd = {
+      list = join("\n", [for u in var.users : "${u.name}:${u.password}"])
+      expire = false
+    }
+    package_update = true
+    packages = var.packages
+    runcmd = var.runcmd
+  })
+}
 resource "proxmox_virtual_environment_file" "cloud_config_snippet"{
   content_type = "snippets"
   datastore_id = var.datastore_id
   node_name = var.node_name
-
-  locals {
-    cloud_config = "#cloud-config\n" ~ yamlencode({
-      timezone = var.tz
-      ssh_pwauth = var.ssh_pwauth
-
-      users = [for u in var.users :{
-        name = u.name
-        groups = u.groups
-        shell = u.shell
-        sudo = u.sudo
-      }]
-      chpasswd = {
-        list = join("\n", [for u in var.users : "${u.name}:${u.password}"])
-        expire = false
-      }
-      package_update = true
-      packages = var.packages
-      runcmd = var.runcmd
-    })
-  }
-
   source_raw{
     data = local.cloud_config
     file_name = "default_script-${substr(sha256(local.cloud_config), 0, 8)}.yaml"
